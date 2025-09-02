587c1563b974b10ccf4082e2bbfa08186b9cf0ce
Elias Pardo <eliaspardo@gmail.com>
2025-09-02 22:35:36 +0200

    Added a couple more tests to RagPreprocessor

--- tests/test_rag_preprocessor.py
+++ tests/test_rag_preprocessor.py
@@ -1,6 +1,13 @@
 import pytest
 from unittest.mock import patch
 from src.rag_preprocessor import RAGPreprocessor
+from langchain.schema import Document
+
+
+TEST_PDF = "tests/data/pdf-test.pdf"
+STRING_LIST = ["test", "string", "for", "testing"]
+EMPTY_LIST = []
+STRING_LIST_EMPTY_CHUNKS = ["test", "    ", "for", "   testing   "]
 
 
 class TestRagPreprocessor:
@@ -19,6 +26,21 @@ class TestRagPreprocessor:
             rag_preprocessor.load_pdf_text()
 
     def test_load_pdf_text_successful(self, rag_preprocessor):
-        texts = rag_preprocessor.load_pdf_text("tests/data/pdf-test.pdf")
+        texts = rag_preprocessor.load_pdf_text(TEST_PDF)
         full_text = "".join(texts)
         assert "Congratulations" in full_text
+
+    def test_split_text_to_docs_success(self, rag_preprocessor):
+        documents = rag_preprocessor.split_text_to_docs(STRING_LIST)
+        assert len(documents) > 0
+        for document in documents:
+            print("document")
+            assert isinstance(document, Document)
+
+    def test_split_text_to_docs_empty_list(self, rag_preprocessor):
+        documents = rag_preprocessor.split_text_to_docs(EMPTY_LIST)
+        assert len(documents) == 0
+
+    def test_split_text_to_docs_empty_chunks(self, rag_preprocessor):
+        documents = rag_preprocessor.split_text_to_docs(STRING_LIST_EMPTY_CHUNKS)
+        assert all(document.page_content.strip() for document in documents)
