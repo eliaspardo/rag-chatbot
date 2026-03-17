a58e77eb3b49223cd89c9ef3f8e42d2c3b2f1571
eliaspardo <eliaspardo@gmail.com>
2026-03-17 20:36:45 +0100

    Adding DMS check to health endpoint.

--- src/inference_service/lifespan.py
+++ src/inference_service/lifespan.py
@@ -1,9 +1,12 @@
 from contextlib import asynccontextmanager
+import os
 
+from src.inference_service.document_management_client import DocumentManagementClient
 from src.inference_service.session_manager import SessionManager
 from src.inference_service.bootstrap import prepare_vector_store
 from src.inference_service.core.vector_store_loader import get_vector_store_loader
 from src.inference_service.core.exam_prep_core import ExamPrepCore
+from src.shared.env_loader import load_environment
 from src.shared.exceptions import (
     ChromaException,
     ServerSetupException,
@@ -15,12 +18,18 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+load_environment()
+DMS_URL = os.getenv("DMS_URL")
+
 
 @asynccontextmanager
 async def lifespan(app):
     # Startup
     print("Loading vector store...")
     app.state.vector_store_loader = get_vector_store_loader()
+    if not DMS_URL:
+        raise ServerSetupException("DMS_URL environment variable is required")
+    app.state.dms_client = DocumentManagementClient(DMS_URL)
     try:
         vectordb = prepare_vector_store(
             vector_store_loader=app.state.vector_store_loader,
--- tests/unit/inference_service/test_api_main.py
+++ tests/unit/inference_service/test_api_main.py
@@ -4,6 +4,7 @@ from unittest.mock import Mock
 from fastapi.testclient import TestClient
 
 from src.inference_service import main as api_main
+from src.shared.constants import DocumentStatus
 
 
 @asynccontextmanager
@@ -22,16 +23,84 @@ def _build_client_no_lifespan():
         api_main.app.router.lifespan_context = original_lifespan
 
 
-def test_health_check():
+def test_health_check_one_document():
     vector_store_loader = Mock()
-    vector_store_loader.get_collection_count.return_value = 5
+    vector_store_loader.get_collection_count.return_value = 1
     api_main.app.state.vector_store_loader = vector_store_loader
+
+    dms_client = Mock()
+    dms_response = [
+        {
+            "doc_hash": "Doc Hash 1",
+            "doc_name": "Doc Name 1",
+            "status": DocumentStatus.PENDING,
+        },
+    ]
+    dms_client.get_documents.return_value = dms_response
+    api_main.app.state.dms_client = dms_client
 
     with _build_client_no_lifespan() as client:
         response = client.get("/health")
 
         assert response.status_code == 200
-        assert response.json() == {"status": "ok", "documents_loaded": "5"}
+        assert response.json() == {
+            "status": "ok",
+            "documents_loaded_in_vector_store": "1",
+            "documents_loaded_in_dms": f"{dms_response}",
+        }
+
+
+def test_health_check_two_documents():
+    vector_store_loader = Mock()
+    vector_store_loader.get_collection_count.return_value = 2
+    api_main.app.state.vector_store_loader = vector_store_loader
+
+    dms_client = Mock()
+    dms_response = [
+        {
+            "doc_hash": "Doc Hash 1",
+            "doc_name": "Doc Name 1",
+            "status": DocumentStatus.PENDING,
+        },
+        {
+            "doc_hash": "Doc Hash 2",
+            "doc_name": "Doc Name 2",
+            "status": DocumentStatus.ERROR,
+        },
+    ]
+    dms_client.get_documents.return_value = dms_response
+    api_main.app.state.dms_client = dms_client
+
+    with _build_client_no_lifespan() as client:
+        response = client.get("/health")
+
+        assert response.status_code == 200
+        assert response.json() == {
+            "status": "ok",
+            "documents_loaded_in_vector_store": "2",
+            "documents_loaded_in_dms": f"{dms_response}",
+        }
+
+
+def test_health_check_no_documents():
+    vector_store_loader = Mock()
+    vector_store_loader.get_collection_count.return_value = 0
+    api_main.app.state.vector_store_loader = vector_store_loader
+
+    dms_client = Mock()
+    dms_response = []
+    dms_client.get_documents.return_value = dms_response
+    api_main.app.state.dms_client = dms_client
+
+    with _build_client_no_lifespan() as client:
+        response = client.get("/health")
+
+        assert response.status_code == 200
+        assert response.json() == {
+            "status": "ok",
+            "documents_loaded_in_vector_store": "0",
+            "documents_loaded_in_dms": f"{dms_response}",
+        }
 
 
 def test_domain_expert_chat_endpoint():
